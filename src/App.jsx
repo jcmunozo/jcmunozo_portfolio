afb9dac99400f3cd16f5ed7fb0d255d9029966b6
jcmunozo <jcmunozo@unicauca.edu.co>
2024-08-20 23:27:52 -0500

    Added Hero and about compoment

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import styles from  './App.module.css'
+import {Navbar} from './components/Navbar/Navbar'
+import {Hero} from './components/Hero/Hero'
+import {About} from './components/About/About'
 
 function App() {
 
-  return <div className={styles.App}>Hi, this is the begining of a frontend developer</div>;
+  return <div className={styles.App}>
+    <Navbar></Navbar>
+    <Hero></Hero>
+    <About></About>
+  </div>;
 }
 
 export default App
